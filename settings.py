dc88280b6db2f21d4cf52a0bc7e00a9d1e1cfdbf
Hylosium <90805628+Hylosium@users.noreply.github.com>
2024-03-05 02:19:43 +0100

    Update name of default images

--- settings.py
+++ settings.py
@@ -25,8 +25,8 @@ SOURCES_LIST = [IMAGE, VIDEO, WEBCAM, RTSP, YOUTUBE]
 
 # Images config
 IMAGES_DIR = ROOT / 'images'
-DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
-DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
+DEFAULT_IMAGE = IMAGES_DIR / 'image_1.jpg'
+DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'image_2.jpg'
 
 # Videos config
 VIDEO_DIR = ROOT / 'videos'
