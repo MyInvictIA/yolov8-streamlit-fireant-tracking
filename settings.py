279d59e187f40452ff20f1d8cca162ee9bf7da16
Hylosium <90805628+Hylosium@users.noreply.github.com>
2024-03-05 02:21:40 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -31,17 +31,13 @@ DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'image_2.jpg'
 # Videos config
 VIDEO_DIR = ROOT / 'videos'
 VIDEO_1_PATH = VIDEO_DIR / 'video_1.mp4'
-VIDEO_2_PATH = VIDEO_DIR / 'video_2.mp4'
-VIDEO_3_PATH = VIDEO_DIR / 'video_3.mp4'
 VIDEOS_DICT = {
-    'video_1': VIDEO_1_PATH,
-    'video_2': VIDEO_2_PATH,
-    'video_3': VIDEO_3_PATH,
+    'video_1': VIDEO_1_PATH
 }
 
 # ML Model config
 MODEL_DIR = ROOT / 'weights'
-DETECTION_MODEL = MODEL_DIR / 'yolov8n.pt'
+#DETECTION_MODEL = MODEL_DIR / 'yolov8n.pt'
 # In case of your custome model comment out the line above and
 # Place your custom model pt file name at the line below 
 # DETECTION_MODEL = MODEL_DIR / 'my_detection_model.pt'
